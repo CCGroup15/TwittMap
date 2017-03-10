5d0c3ff4ec4c82d4d9ac2e193240b115b6a11815
xu2333 <zicheng.xu@columbia.edu>
2017-03-10 17:06:16 -0500

    Download process

---
+++ config.py
@@ -0,0 +1,12 @@
+import googlemaps
+
+consumer_key = 'k20CumsFv7l5JO0KqVIT03DeL'
+consumer_secret = 'V57rHUlKcijBPzU6GlyYKNPQzjwM7bJEOmdKlTfWNEixh0y4er'
+access_token = '4604284034-ZlWxWhxT9UTtNX42SmehU63XoA7ZsUGtyWxBWfn'
+access_secret = 'HNbiT0w6UWiNxmhiGPhEI5xDR3kEKkGyswVmNSwTDA9ot'
+
+GMAP_KEY = 'AIzaSyAovkBO68rNju_an3XHa29XmUQ0RWq55Is'
+
+gmaps = googlemaps.Client(key=GMAP_KEY)
+
+FILTERS = ['china', 'usa', 'japan', 'russia', 'england', 'france', 'german']
