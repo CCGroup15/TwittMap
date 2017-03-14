8d5b10e4534b5b59f2f0e908e8de5f4a9bc366ae
xu2333 <zicheng.xu@columbia.edu>
2017-03-14 18:38:58 -0400

    render

--- application.py
+++ application.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import config
+import json
 
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# print a nice greeting.
 @application.route('/')
 def say_hello():
+    def index():
+        coords = []
+        return render_template("index.html", coords=json.dumps(coords))
     return render_template('index.html')
 
-# run the app.
+@application.route('/result', methods = ['GET'])
+def result():
+    if request.method == 'GET':
+        category = request.args.getlist("category")
+        # print(category)
+        coords = []
+        for keyword in category:
+            es_data = config.es.search(index=config.AWS_ES_INDEX, body={"query": {"match": {"text": keyword}}}, size=600)
+            for data in es_data['hits']['hits']:
+                if len(data['_source']['coordinates']) > 0:
+                    geo_data = data['_source']['coordinates']['location'].split(',')
+                    lat = float(geo_data[0])
+                    lng = float(geo_data[1])
+                    coords.append([lat, lng])
+        print(coords)
+        return render_template("render.html", coords=json.dumps(coords))
+
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
